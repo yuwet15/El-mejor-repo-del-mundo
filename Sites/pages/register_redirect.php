3a4fc4fc8ce5d9ec751525c5f11204074762d30f
ChengYuwen <ycheng@uc.cl>
2021-07-05 12:35:16 -0400

    cambio

--- Sites/pages/register_redirect.php
+++ Sites/pages/register_redirect.php
@@ -15,7 +15,9 @@ if (isset($_POST['register'])) {
     $result -> execute();
 
     $result = $result -> fetchAll();
-    echo($result[0][0]);
+    
+
+
     if ($result[0][0] == 'TRUE') {
         $_SESSION['register'] = 'TRUE';
         header("Location: login.php");
