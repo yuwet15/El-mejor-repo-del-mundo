db21931e123b89a39aeaaa53fd34a446c80a801b
ChengYuwen <ycheng@uc.cl>
2021-07-04 22:17:33 -0400

    cambios

--- Entrega3/insertar_en_tabla.sql
+++ Entrega3/insertar_en_tabla.sql
@@ -13,7 +13,7 @@ BEGIN
   -- control de flujo
   	IF rut_u NOT IN (SELECT rut_p FROM Usuario_pass) THEN
 		INSERT INTO Usuario_pass(rut_p, password_p)
-		SELECT rut, substring(rut,5,4);
+		SELECT rut_u, substring(rut_u,5,4);
 	END IF;
 
 -- -- finalizamos la definición de la función y declaramos el lenguaje
