c3c4410552f43ff7224f98013aa35c98173c808a
ChengYuwen <ycheng@uc.cl>
2021-07-05 14:00:30 -0400

    cambio

--- Sites/pages/register_redirect.php
+++ Sites/pages/register_redirect.php
@@ -16,7 +16,7 @@ if (isset($_POST['register'])) {
     $result -> execute();
 
     $result = $result -> fetchAll();
-    echo($result[0][0])
+    echo($result[0][0]);
     if ($result[0][0] == 'TRUE') {
         $query = "SELECT insertar_en_tabla('$rut');";
         $result = $db -> prepare($query);
