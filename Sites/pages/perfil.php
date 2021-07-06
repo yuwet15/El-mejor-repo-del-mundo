21212c05656cccad4d632d9140265233eaf18264
JOSÉ DOMINGO SILVA <jdsilvam@uc.cl>
2021-07-06 14:36:40 -0400

    d2

--- Sites/pages/perfil.php
+++ Sites/pages/perfil.php
@@ -12,58 +12,66 @@ if (isset($_SESSION['rut'])){
 }
 require("../config/conexion.php");
 
-$query = "SELECT DISTINCT nombre, edad, rut
-          FROM usuarios
-          WHERE rut = '".$_SESSION['rut']."'";
+if (isset($_SESSION['rut'])){
+  if(isset($_SESSION['jefe'])){
+    $query = "SELECT DISTINCT nombre, edad, rut
+    FROM usuarios
+    WHERE rut = '".$_SESSION['rut']."'";
 
-$result = $db -> prepare($query);
-$result -> execute();
-$user_info = $result -> fetchAll();
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $user_info = $result -> fetchAll();
 
-$query = "SELECT DISTINCT c.direccion
-          FROM direcciones AS d, usuarios as u, comunas as c
-          WHERE u.usuario_id = d.usuario_id
-          AND d.direccion_id = c.direccion_id
-          AND u.rut = '".$_SESSION['rut']."'";
+    $query = "SELECT DISTINCT c.direccion
+              FROM direcciones AS d, usuarios as u, comunas as c
+              WHERE u.usuario_id = d.usuario_id
+              AND d.direccion_id = c.direccion_id
+              AND u.rut = '".$_SESSION['rut']."'";
 
-$result = $db -> prepare($query);
-$result -> execute();
-$user_address = $result -> fetchAll();
-?>
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $user_address = $result -> fetchAll();
+    ?>
 
-<table class="table">
-  
-  <thead>
+    <table class="table">
+
+    <thead>
     <tr>
     <th>Nombre</th>
     <th>Edad</th>
     <th>Rut</th>
     </tr>
-  </thead>
-  
-  <tbody>
-    <?php
-      foreach ($user_info as $i) {
-          echo "<tr> <td>$i[0]</td> <td>$i[1]</td> 
-                <td>$i[2]</td> </tr>";
-      }
-    ?>
-  </tbody>
-</table>
+    </thead>
 
-<table class="table">
-  
-  <thead>
+    <tbody>
+    <?php
+    foreach ($user_info as $i) {
+        echo "<tr> <td>$i[0]</td> <td>$i[1]</td> 
+              <td>$i[2]</td> </tr>";
+    }
+    ?>
+    </tbody>
+    </table>
+
+    <table class="table">
+
+    <thead>
     <tr>
     <th>Direcciones registradas</th>
     </tr>
-  </thead>
-  
-  <tbody>
+    </thead>
+
+    <tbody>
     <?php
-      foreach ($user_address as $a) {
-          echo "<tr> <td>$a[0]</td> </tr>";
-      }
+    foreach ($user_address as $a) {
+        echo "<tr> <td>$a[0]</td> </tr>";
+    }
     ?>
-  </tbody>
-</table>
+    </tbody>
+    </table>
+  } else {
+      
+  }   
+} else {
+  
+}
