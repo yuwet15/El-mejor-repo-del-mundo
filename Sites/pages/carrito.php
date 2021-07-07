04cbbf24189d06f4727e9af9d4c602c59b507a3f
yuwet15 <ycheng@uc.cl>
2021-07-07 18:44:11 -0400

    Update carrito.php

--- Sites/pages/carrito.php
+++ Sites/pages/carrito.php
@@ -10,5 +10,23 @@ if (isset($_SESSION['rut'])){
 
 
 <div class="container">
+  <table class="table">
   
+	  <thead>
+	    <tr>
+	    <th>Producto</th>
+	    <th>Cantidad</th>
+	    <th>Tienda</th>
+	    </tr>
+	  </thead>
+	  
+	  <tbody>
+		<tr> 
+			<td>$i[0]</td> 
+			<td>$i[1]</td> 
+	        <td>$i[2]</td>
+	    </tr>
+	  </tbody>
+	</table>
+
 </div>
