e85b87e9f61f7a442a617b2109c27cb7548a5a4d
JOSÉ DOMINGO SILVA <jdsilvam@uc.cl>
2021-07-06 20:45:04 -0400

    xd

--- Entrega3/transferir_usuario.sql
+++ Entrega3/transferir_usuario.sql
@@ -1,13 +1,20 @@
 CREATE OR REPLACE FUNCTION
 
-transferir_usuario(id integer, nombre varchar, rut varchar, edad varchar, sexo varchar, cargo varchar)
+transferir_usuario(nombre varchar, rut varchar, edad varchar, sexo varchar, cargo varchar)
 
 RETURNS VOID AS $$
 
+DECLARE
+	idmax int;
+
 BEGIN
   
+	SELECT INTO idmax
+	MAX(usuario_id)
+	FROM usuarios;
+
   IF rut NOT IN (SELECT rut FROM usuarios) THEN
-		INSERT INTO personal values (id, nombre, rut, edad, sexo, NULL, cargo);
+		INSERT INTO personal values (idmax + 1, nombre, rut, edad, sexo, NULL, cargo);
 	END IF;
 
 END
