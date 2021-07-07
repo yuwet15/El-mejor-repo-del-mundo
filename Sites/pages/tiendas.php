3f736827acf25ed8929aaac8a05b03965c0a2871
Yehuda Gurovich <yehuda.gurovich@uc.cl>
2021-07-06 21:04:03 -0400

    Tiendas probando

--- Sites/pages/tiendas.php
+++ Sites/pages/tiendas.php
@@ -11,3 +11,27 @@ if (isset($_SESSION['rut'])){
     include('../templates/body_prelogin.html');
 }
 ?>
+<?php
+require("../config/conexion.php");
+
+$query = "SELECT nombre FROM tiendas;";
+
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$tiendas = $result -> fetchAll();
+  ?>
+
+<table>
+    <tr>
+      <th>Tiendas</th>
+    </tr>
+  
+      <?php
+        foreach ($tiendas as $t) {
+          echo "<tr><td>$t[0]</td></tr>";
+      }
+      ?>
+      
+  </table>
+
+
