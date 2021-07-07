782f0a42e57991a4fc0817febce7a92d34cfbdcd
JOSÉ DOMINGO SILVA <jdsilvam@uc.cl>
2021-07-07 14:49:24 -0400

    xd

--- Sites/pages/productos.php
+++ Sites/pages/productos.php
@@ -13,45 +13,45 @@ include('config/conexion.php');
 $product_id = $_GET['id'];
 
 $query = "SELECT p.tipo
-					FROM productos AS p
-					WHERE p.producto_id = $product_id";
+          FROM productos AS p
+          WHERE p.producto_id = $product_id";
 
 $result = $db -> prepare($query);
 $result -> execute();
 $tipo = $result -> fetchAll();
 
-if $tipo[0][0] == 'Comestible' {
+if ($tipo[0][0] == 'Comestible') {
 	
 	$query = "SELECT c.categoria
-						FROM comestibles AS c
-						WHERE c.producto_id = $product_id";
+            FROM comestibles AS c
+            WHERE c.producto_id = $product_id";
 
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$categoria = $result -> fetchAll();
 
-	if $categoria[0][0] == 'congelado' {
+	if ($categoria[0][0] == 'congelado') {
 	
 		$query = "SELECT p.producto_id, p.nombre, p.precio, p.descripcion,
-		                 com.expiracion, com.categoria, con.peso
-							FROM productos AS p, comestibles AS com, congelados AS con
-							WHERE p.producto_id = com.producto_id
-							AND p.producto_id = con.producto_id 
-							AND p.producto_id = $product_id";
-	
+		          		 com.expiracion, com.categoria, con.peso
+              FROM productos AS p, comestibles AS com, congelados AS con
+              WHERE p.producto_id = com.producto_id
+              AND p.producto_id = con.producto_id 
+              AND p.producto_id = $product_id";
+
 		$result = $db -> prepare($query);
 		$result -> execute();
 		$datos = $result -> fetchAll();
 	
-	} elseif $categoria[0][0] == 'conserva' {
+	} elseif ($categoria[0][0] == 'conserva') {
 	
 		$query = "SELECT p.producto_id, p.nombre, p.precio, p.descripcion,
 		                 com.expiracion, com.categoria, con.conserva
-							FROM productos AS p, comestibles AS com, conserva AS con
-							WHERE p.producto_id = com.producto_id
-							AND p.producto_id = con.producto_id 
-							AND p.producto_id = $product_id";
-	
+              FROM productos AS p, comestibles AS com, conserva AS con
+              WHERE p.producto_id = com.producto_id
+              AND p.producto_id = con.producto_id 
+              AND p.producto_id = $product_id";
+      
 		$result = $db -> prepare($query);
 		$result -> execute();
 		$datos = $result -> fetchAll();
@@ -94,16 +94,16 @@ if $tipo[0][0] == 'Comestible' {
 		<th>Descripción</th>
     
 		<?php
-    if $tipo[0][0] == 'Comestible' {
+    if ($tipo[0][0] == 'Comestible') {
       
 			echo "<th>Fecha de expiración</th>
 						<th>Categoría</th>";
 			
-			if $categoria[0][0] == 'congelado' {
+			if ($categoria[0][0] == 'congelado') {
 				
 				echo "<th>peso(kg)</th>";
 
-			} elseif $categoria[0][0] == 'conserva' {
+			} elseif ($categoria[0][0] == 'conserva') {
 				
 				echo "<th>Tipo de conserva</th>";
 
