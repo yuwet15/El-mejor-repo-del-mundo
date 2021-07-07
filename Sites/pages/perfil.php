9d8f89945c02ed75f7b6aace49f1badbb692af94
yuwet15 <ycheng@uc.cl>
2021-07-07 00:18:07 -0400

    Update perfil.php

--- Sites/pages/perfil.php
+++ Sites/pages/perfil.php
@@ -9,32 +9,59 @@ if (isset($_SESSION['rut'])){
 }
 require("../config/conexion.php");
 
-$query = "SELECT nombre, edad, rut
-          FROM usuarios
-          WHERE rut = '".$_SESSION['rut']."'";
 
-$result = $db -> prepare($query);
-$result -> execute();
-$user_info = $result -> fetchAll();
+if (isset($_SESSION['jefe'])){
+  $query = "SELECT u.nombre, u.edad, u.rut, c.direccion
+            FROM direcciones AS d, usuarios AS u, comunas AS c
+            WHERE u.usuario_id = d.usuario_id
+            AND d.direccion_id = c.direccion_id
+            AND u.rut = '".$_SESSION['rut']."'";
 
-$query = "SELECT DISTINCT c.direccion
-          FROM direcciones AS d, usuarios as u, comunas as c
-          WHERE u.usuario_id = d.usuario_id
-          AND d.direccion_id = c.direccion_id
-          AND u.rut = '".$_SESSION['rut']."'";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $user_info = $result -> fetchAll();
 
-$result = $db -> prepare($query);
-$result -> execute();
-$user_address = $result -> fetchAll();
+  $query = "SELECT p.nombre, p.edad, p.rut 
+            FROM personal AS p, administracion AS a
+            WHERE p.id = a.id
+            AND p.rut != '".$_SESSION['rut']."'
+            AND a.unidad_id IN (
+            SELECT DISTINCT u.id
+            FROM unidades as u, personal as p
+            WHERE u.jefe_id = p.id
+            AND p.rut = '".$_SESSION['rut']."')";
 
-if(!$user_info){
+  $result = $db2 -> prepare($query);
+  $result -> execute();
+  $empleados = $result -> fetchAll();
+}else{
   $query = "SELECT nombre, edad, rut
-            FROM Personal
+            FROM usuarios
             WHERE rut = '".$_SESSION['rut']."'";
 
   $result = $db -> prepare($query);
   $result -> execute();
   $user_info = $result -> fetchAll();
+
+  $query = "SELECT DISTINCT c.direccion
+            FROM direcciones AS d, usuarios as u, comunas as c
+            WHERE u.usuario_id = d.usuario_id
+            AND d.direccion_id = c.direccion_id
+            AND u.rut = '".$_SESSION['rut']."'";
+
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $user_address = $result -> fetchAll();
+
+  if(!$user_info){
+    $query = "SELECT nombre, edad, rut
+              FROM Personal
+              WHERE rut = '".$_SESSION['rut']."'";
+
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $user_info = $result -> fetchAll();
+  }
 }
 
 ?>
@@ -46,6 +73,10 @@ if(!$user_info){
     <th>Nombre</th>
     <th>Edad</th>
     <th>Rut</th>
+    <?php
+    if(isset($_SESSION['jefe'])){
+      echo "<th>>Dirección de mi unidad</th>"
+    }?>
     </tr>
   </thead>
   
@@ -53,7 +84,11 @@ if(!$user_info){
     <?php
       foreach ($user_info as $i) {
           echo "<tr> <td>$i[0]</td> <td>$i[1]</td> 
-                <td>$i[2]</td> </tr>";
+                <td>$i[2]</td>"
+          if(isset($_SESSION['jefe'])){
+            echo "<td>$i[3]</td>"
+          }
+          echo "</tr>";
       }
     ?>
   </tbody>
@@ -61,7 +96,7 @@ if(!$user_info){
 
 <?php
 if(!$user_address){
-  echo "Usuario No presenta direcciones registradas";
+  echo "Usuario no presenta direcciones registradas";
 }else{
   echo "<table class=\"table\">";
     
@@ -80,4 +115,11 @@ if(!$user_address){
     echo "</tbody>";
   echo "</table>";
 }
+
+if(isset($_SESSION['jefe'])){
+
+}
+
+
+
 ?>
