ff6602802493477e246f3e7222fbcf184655ca0e
yuwet15 <ycheng@uc.cl>
2021-07-07 13:26:08 -0400

    Update login_redirect.php

--- Sites/pages/login_redirect.php
+++ Sites/pages/login_redirect.php
@@ -25,7 +25,7 @@ if (isset($_POST['login'])) {
      
         $result = $result -> fetchAll();
         if(!$result[0][0]){
-            //nothing
+            echo("hehe");
         }elseif($result[0][0] == 'administracion'){
             $_SESSION['jefe'] = TRUE;
         }elseif ($result[0][0] != 'usuario') {
