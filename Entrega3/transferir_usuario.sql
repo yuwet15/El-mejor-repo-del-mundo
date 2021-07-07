c58b6be2e5574f4737dcfa5d3e024fb6a527c6a6
JOSÉ DOMINGO SILVA <jdsilvam@uc.cl>
2021-07-06 20:39:24 -0400

    xd

--- Entrega3/transferir_usuario.sql
+++ Entrega3/transferir_usuario.sql
@@ -7,7 +7,7 @@ RETURNS VOID AS $$
 BEGIN
   
   IF rut NOT IN (SELECT rut FROM usuarios) THEN
-		INSERT INTO personal values (id, nombre, rut, edad, sexo, NULL, cargo)
+		INSERT INTO personal values (id, nombre, rut, edad, sexo, NULL, cargo);
 	END IF;
 
 END
