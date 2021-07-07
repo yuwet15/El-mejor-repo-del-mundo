99892979a84a6394c993673121469dc6d1c332c8
Yehuda Gurovich <yehuda.gurovich@uc.cl>
2021-07-07 12:27:39 -0400

    tiendas probando

--- Sites/pages/tiendas2.php
+++ Sites/pages/tiendas2.php
@@ -11,7 +11,13 @@ if (isset($_SESSION['rut'])){
 <form class="row g-4 justify-content-center" name="form" id="form" method="post" action="">
   <div class='col-auto' style="text-align:center">
     <br>
-    <p >Mostrar los 3 productos mas baratos por categoría</p>
-    <button type="button" class="btn btn-dark">Mostrar</button>
+    Mostrar los 3 productos mas baratos por categoría
+    <input type="submit" value="Mostrar">
   </div>
+</form>
+
+<form class="row g-4 justify-content-center" name="form" id="form" method="post" action="">
+  Nombre del producto:
+  <input type="text" name="nombre">
+  <input type="submit" value="Buscar">
 </form>
