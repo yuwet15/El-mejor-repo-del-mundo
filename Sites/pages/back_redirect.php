08641a7c9bd12785237d02e691d060ebc9f298b1
yuwet15 <ycheng@uc.cl>
2021-07-07 17:30:24 -0400

    Update back_redirect.php

--- Sites/pages/back_redirect.php
+++ Sites/pages/back_redirect.php
@@ -3,9 +3,9 @@ session_start();
 include('../config/conexion.php');
 
  
-if (isset($_POST['previous_location'])){
-	$previous_location = $_POST['previous_location'];
-	unset($_POST['previous_location']);
+if (isset($_SESSION['previous_location'])){
+	$previous_location = $_SESSION['previous_location'];
+	unset($_SESSIONT['previous_location']);
 	header("Location: $previous_location");
 } else{
 	header("Location: ../index.php");
