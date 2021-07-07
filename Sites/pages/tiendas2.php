d6ff2c89644e0b30c56b40b7a417c9985c3e5507
Yehuda Gurovich <yehuda.gurovich@uc.cl>
2021-07-07 12:31:06 -0400

    tiendas probando

--- Sites/pages/tiendas2.php
+++ Sites/pages/tiendas2.php
@@ -14,11 +14,11 @@ if (isset($_SESSION['rut'])){
     Mostrar los 3 productos mas baratos por categoría
     <input type="submit" value="Mostrar">
   </div>
-</form>
 
-<form class="row g-4 justify-content-center" name="form" id="form" method="post" action="">
+  <div class='col-auto' style="text-align:center">
   <br>
-  Nombre del producto:
-  <input type="text" name="nombre">
-  <input type="submit" value="Buscar">
+    Nombre del producto:
+    <input type="text" name="nombre">
+    <input type="submit" value="Buscar">
+  </div>
 </form>
