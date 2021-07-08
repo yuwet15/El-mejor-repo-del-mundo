c784fd3841b5ec3adeb02dd804851d686a07a15e
yuwet15 <ycheng@uc.cl>
2021-07-07 20:48:58 -0400

    Update tiendas2.php

--- Sites/pages/tiendas2.php
+++ Sites/pages/tiendas2.php
@@ -203,6 +203,7 @@ if (isset($_SESSION['rut'])){
     $result -> execute();
  
     $result = $result -> fetchAll();
+    echo($result[0][0]);
     if(!$result[0][1]){
       $query = "INSERT INTO carrito(rut, tienda_id, producto_id, cantidad)
                 SELECT $rut_session, $id, $id_producto, $cantidad";
@@ -217,6 +218,7 @@ if (isset($_SESSION['rut'])){
       $result -> execute();
     }
     echo "Agregado exitosamente al carrito";
+    unset($_POST['buscar_i']);
   }
 ?>
 
