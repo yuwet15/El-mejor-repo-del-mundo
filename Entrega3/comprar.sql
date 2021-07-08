f35c99141e653e20171e6b911f4eedffe7e75936
JOSÉ DOMINGO SILVA <jdsilvam@uc.cl>
2021-07-07 21:07:07 -0400

    xd

--- Entrega3/comprar.sql
+++ Entrega3/comprar.sql
@@ -1,18 +1,27 @@
 CREATE OR REPLACE FUNCTION
 
-comprar(rut_u varchar, direccion_id integer, tienda_id integer)
+comprar(rut_u varchar, tienda_id integer, direccion_id integer, producto_id integer, cantidad integer)
 
 RETURNS VOID AS $$
 
 DECLARE
-  max_id int;
+  idmax int;
+  id_usuario int;
 
 BEGIN
 
-  	IF rut_u NOT IN (SELECT rut_p FROM Usuario_pass) THEN
-		INSERT INTO Usuario_pass(rut_p, password_p)
-		SELECT rut_u, substring(rut_u,5,4);
-	END IF;
+    SELECT INTO idmax
+    MAX(compra_id)
+    FROM compras;
+
+    SELECT INTO id_usuario
+    usuario_id
+    FROM usuarios
+    WHERE rut = rut_u;
+
+		INSERT INTO compras(idmax + 1, id_usuario, tienda_id, direccion_id);
+    INSERT INTO detalle(idmax + 1, producto_id, cantidad);
 
 END
+
 $$ language plpgsql
