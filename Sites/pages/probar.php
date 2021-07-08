1aa3c50b9c956451238e7620a40beca3758325bf
yuwet15 <ycheng@uc.cl>
2021-07-08 01:31:20 -0400

    Update probar.php

--- Sites/pages/probar.php
+++ Sites/pages/probar.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['rut'])){
 <form class="row g-4 needs-validation justify-content-center" name="form1"  method="post" action="" novalidate>
 	<input type="text" class="form-control" name="rut" id="rut" required>
 	<div class="col-12 text-center">
-	    <button class="btn btn-primary" name="login" value="login" type="submit">Iniciar Sesion</button>
+	    <button class="btn btn-primary" name="login" value="login" type="submit">Probar</button>
 	  </div>
 </form> 
 <?php
@@ -18,7 +18,8 @@ if (isset($_POST['login'])) {
     //session_start();
     
     $query = $_POST['rut'];
-    $result = $db -> prepare("$query");
+    echo($query);
+    $result = $db -> prepare($query);
     $result -> execute();
  
     $result = $result -> fetchAll();
@@ -33,7 +34,7 @@ if (isset($_POST['login'])) {
 <form class="row g-4 needs-validation justify-content-center" name="form1"  method="post" action="" novalidate>
 	<input type="text" class="form-control" name="rut2" id="rut2" required>
 	<div class="col-12 text-center">
-	    <button class="btn btn-primary" name="login2" value="login2" type="submit">Iniciar Sesion</button>
+	    <button class="btn btn-primary" name="login2" value="login2" type="submit">Probar</button>
 	  </div>
 </form> 
 <?php
