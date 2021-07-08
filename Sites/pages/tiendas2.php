a1bb9ad9b94cbc1918434756709d02057b223a7a
Yehuda Gurovich <yehuda.gurovich@uc.cl>
2021-07-07 21:48:06 -0400

    tiendas probando

--- Sites/pages/tiendas2.php
+++ Sites/pages/tiendas2.php
@@ -28,7 +28,6 @@ if (isset($_SESSION['rut'])){
 
 <div class="center">
   <div class="accordion accordion-flush" id="mostrar_p">
-  <div class="espacio"></div>
   <div class="accordion-item" style="background-color:#DCE1E3">
     <h2 class="accordion-header" id="headingOne">
       <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" >
