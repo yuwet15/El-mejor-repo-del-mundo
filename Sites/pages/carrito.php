1ec8a872cda292eaa54258a94e9a836c94844237
Yehuda Gurovich <yehuda.gurovich@uc.cl>
2021-07-07 22:35:42 -0400

    tiendas probando

--- Sites/pages/carrito.php
+++ Sites/pages/carrito.php
@@ -1,8 +1,6 @@
 <?php 
 session_start();
 
-echo "<body style='background-color:#E6EAEB'>";
-
 if (isset($_SESSION['rut'])){
   include('../templates/header.html');   
   include('../templates/body_postlogin.html'); 
