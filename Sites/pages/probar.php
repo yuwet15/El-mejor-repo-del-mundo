05e07ed3163050f1ca3338ca518bd915561eee73
yuwet15 <ycheng@uc.cl>
2021-07-08 01:38:04 -0400

    Update probar.php

--- Sites/pages/probar.php
+++ Sites/pages/probar.php
@@ -12,24 +12,42 @@ if (isset($_SESSION['rut'])){
 	<div class="col-12 text-center">
 	    <button class="btn btn-primary" name="login" value="login" type="submit">Probar</button>
 	  </div>
-</form> 
-<?php
-if (isset($_POST['login'])) {   
-    //session_start();
+</form>
+
+<table class="table">
     
-    $query = $_POST['rut'];
-    echo($query);
-    $result = $db -> prepare($query);
-    $result -> execute();
- 
-    $result = $result -> fetchAll();
-    foreach ($result as $r) {
-    	foreach ($r as $k) {
-    		echo($k);
-    	}
-    }
-}
-?>
+    <thead>
+      <tr>
+	      <th>1</th>
+	      <th>2</th>
+	      <th>3</th> 
+	      <th>4</th> 
+	      <th>5</th> 
+	      <th>6</th> 
+	      <th>7</th> 
+	      <th>8</th> 
+  		</tr>
+  		</thead>
+    
+    <tbody>
+    	<?php
+			if (isset($_POST['login'])) {   
+			    //session_start();
+			    
+			    $query = $_POST['rut'];
+			    echo($query);
+			    $result = $db -> prepare($query);
+			    $result -> execute();
+			 
+			    $result = $result -> fetchAll();
+			    foreach ($result as $i) {
+			    	echo "<tr> <td>$i[0]</td> <td>$i[1]</td> <td>$i[2]</td> <td>$i[3]</td> <td>$i[4]</td> <td>$i[5]</td> <td>$i[6]</td> <td>$i[7]</td></tr>";
+			    }
+			}
+			?>
+    </tbody>
+  </table>
+
 
 <form class="row g-4 needs-validation justify-content-center" name="form1"  method="post" action="" novalidate>
 	<input type="text" class="form-control" name="rut2" id="rut2" required>
@@ -37,19 +55,37 @@ if (isset($_POST['login'])) {
 	    <button class="btn btn-primary" name="login2" value="login2" type="submit">Probar</button>
 	  </div>
 </form> 
-<?php
-if (isset($_POST['login2'])) {   
-    //session_start();
+
+<table class="table">
     
-    $query = $_POST['rut2'];
-    $result = $db -> prepare("$query");
-    $result -> execute();
- 
-    $result = $result -> fetchAll();
-    foreach ($result as $r) {
-    	foreach ($r as $k) {
-    		echo($k);
-    	}
-    }
-}
-?>
+    <thead>
+      <tr>
+	      <th>1</th>
+	      <th>2</th>
+	      <th>3</th> 
+	      <th>4</th> 
+	      <th>5</th> 
+	      <th>6</th> 
+	      <th>7</th> 
+	      <th>8</th> 
+  		</tr>
+  		</thead>
+    
+    <tbody>
+    	<?php
+			if (isset($_POST['login2'])) {   
+			    //session_start();
+			    
+			    $query = $_POST['rut2'];
+			    echo($query);
+			    $result = $db2 -> prepare($query);
+			    $result -> execute();
+			 
+			    $result = $result -> fetchAll();
+			    foreach ($result as $i) {
+			    	echo "<tr> <td>$i[0]</td> <td>$i[1]</td> <td>$i[2]</td> <td>$i[3]</td> <td>$i[4]</td> <td>$i[5]</td> <td>$i[6]</td> <td>$i[7]</td></tr>";
+			    }
+			}
+			?>
+    </tbody>
+  </table>
