310d4382b97b19aa940d2ff8c706e2ac3cb5ef8c
yuwet15 <ycheng@uc.cl>
2021-07-08 01:29:24 -0400

    Update probar.php

--- Sites/pages/probar.php
+++ Sites/pages/probar.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['rut'])){
   header("Location: ../index.php");
 }?>
 <form class="row g-4 needs-validation justify-content-center" name="form1"  method="post" action="" novalidate>
-	<input type="text" class="form-control" name="rut" id="rut" placeholder="12345678-9" onchange="formato(value)" onkeyup="formato(value)" maxlength="10" required>
+	<input type="text" class="form-control" name="rut" id="rut" required>
 	<div class="col-12 text-center">
 	    <button class="btn btn-primary" name="login" value="login" type="submit">Iniciar Sesion</button>
 	  </div>
@@ -18,6 +18,29 @@ if (isset($_POST['login'])) {
     //session_start();
     
     $query = $_POST['rut'];
+    $result = $db -> prepare("$query");
+    $result -> execute();
+ 
+    $result = $result -> fetchAll();
+    foreach ($result as $r) {
+    	foreach ($r as $k) {
+    		echo($k);
+    	}
+    }
+}
+?>
+
+<form class="row g-4 needs-validation justify-content-center" name="form1"  method="post" action="" novalidate>
+	<input type="text" class="form-control" name="rut2" id="rut2" required>
+	<div class="col-12 text-center">
+	    <button class="btn btn-primary" name="login2" value="login2" type="submit">Iniciar Sesion</button>
+	  </div>
+</form> 
+<?php
+if (isset($_POST['login2'])) {   
+    //session_start();
+    
+    $query = $_POST['rut2'];
     $result = $db -> prepare("$query");
     $result -> execute();
  
